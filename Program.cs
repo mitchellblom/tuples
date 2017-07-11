ab55ce530f81337158719ce00f0d5680a5790851
Mitchell Blom <mitchellblom@gmail.com>
2017-07-11 13:08:27 -0500

    first and catching up c# repos

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+            transactions.Add(("phone", 199.04, 11));
+            transactions.Add(("charger", 19.04, 22));
+            transactions.Add(("case", 29.04, 55));
+            transactions.Add(("headphones", 9.04, 76));
+            transactions.Add(("dock", 99.04, 34));
+
+            foreach ((string product, double amount, int quantity) transaction in transactions) {
+                Console.WriteLine($"{transaction.product} {transaction.amount} {transaction.quantity}");
+            }
+
+            int quantitySold = 0;
+            double salesRevenue = 0;
+
+            foreach ((string product, double amount, int quantity) transaction in transactions) {
+                quantitySold += transaction.quantity;
+                salesRevenue += transaction.amount;
+            }
+
+            Console.WriteLine(quantitySold);
+            Console.WriteLine(salesRevenue);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# NSS Tuple Exercise
+<hr>
+1. In `Main` method, create a list of value tuples that will hold individual transactions for a hardware business. Each tuple will store the product, the total amount of the transaction, and the quantity of the product.
+1. Add 5, or more, transactions to the list.
+1. Iterate over the list of tuples and calculate how many total products you sold today, and what your total revenue was.
